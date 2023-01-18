32a930bb8ce89b2b22dc4b93a4a080018d6019d5
samvrny <samvrny@gmail.com>
2023-01-17 23:01:45 -0600

    wrote basic game logic to flip and check if cards are a match

--- index.js
+++ index.js
@@ -4,64 +4,95 @@ document.addEventListener('DOMContentLoaded', () => {
     const pictures = [
         {
             name: 'blueorange',
-            img: './images/blueorange.png'
+            img: 'images/blueorange.png'
         },
         {
             name: 'blueorange',
-            img: './images/blueorange.png'
+            img: 'images/blueorange.png'
         },
         {
             name: 'eggs',
-            img: './images/eggs.png'
+            img: 'images/eggs.png'
         },
         {
             name: 'eggs',
-            img: './images/eggs.png'
+            img: 'images/eggs.png'
         },
         {
             name: 'monkeys',
-            img: './images/monkeys.png'
+            img: 'images/monkeys.png'
         },
         {
             name: 'monkeys',
-            img: './images/monkeys.png'
+            img: 'images/monkeys.png'
         },
         {
             name: 'rose',
-            img: './images/rose.png'
+            img: 'images/rose.png'
         },
         {
             name: 'rose',
-            img: './images/rose.png'
+            img: 'images/rose.png'
         },
         {
             name: 'leaves',
-            img: './images/leaves.png'
+            img: 'images/leaves.png'
         },
         {
             name: 'leaves',
-            img: './images/leaves.png'
+            img: 'images/leaves.png'
         },
         {
-            name: 'hotballons',
-            img: './images/hotballons.png'
+            name: 'hotballoons',
+            img: 'images/hotballoons.png'
         },
         {
-            name: 'hotballons',
-            img: './images/hotballons.png'
+            name: 'hotballoons',
+            img: 'images/hotballoons.png'
         }
     ]
 
     //make game board
     const gameBox = document.getElementById('grid')
+    let chosenCards = [];
+    let chosenCardsId = [];
+    let wonCards = [];
 
     function loadGame() {
         for(let i = 0; i < pictures.length; i++) {
             let card = document.createElement('img');
             card.setAttribute('src', 'images/blueback.png')
             card.setAttribute('data-id', i);
-            // card.addEventListener('click', flipCard)
+            card.addEventListener('click', flipCard)
             gameBox.appendChild(card)
+        }
+    }
+
+    function checkMatch() {
+        let cardsInQuestion = document.querySelectorAll('img');
+        const cardOneId = chosenCardsId[0];
+        const cardTwoId = chosenCardsId[1];
+        if(chosenCards[0] === chosenCards[1]) {
+            console.log('MATCH BIATCH!!')
+            cardsInQuestion[cardOneId].removeAttribute('src');
+            cardsInQuestion[cardTwoId].removeAttribute('src');
+            wonCards.push(chosenCards)
+        } else {
+            cardsInQuestion[cardOneId].setAttribute('src', 'images/blueback.png')
+            cardsInQuestion[cardTwoId].setAttribute('src', 'images/blueback.png')
+        }
+        chosenCards = [];
+        chosenCardsId = [];
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id');
+        chosenCards.push(pictures[cardId].name);
+        chosenCardsId.push(cardId);
+        this.setAttribute('src', pictures[cardId].img)
+
+        if(chosenCards.length === 2) {
+            setTimeout(checkMatch, 750)
         }
     }
 
